b0fcdfd255d7532ab85a0d872b8a7b824e8a3509
Theo Andersen Carton <andersen.theo@gmail.com>
2015-07-17 22:29:16 +0200

    Added a spare test

--- bowling.rb
+++ bowling.rb
@@ -2,26 +2,35 @@ require "test/unit"
 
 class Game
   def initialize()
+      @pins = []
   end
 
   def throw(pins)
-    if @score.nil?
-      @score = pins
-    else
-      @score.add(pins)
-    end
+    @pins = @pins.push(pins)
   end
 
   def score
-    return @score
+    allPins = Pins.new(0)
+
+    if @pins.length == 0
+      return allPins
+    end
+
+    for i in 0..@pins.length-1
+      allPins.add(@pins[i])
+
+      if i > 0 && (@pins[i].pins + @pins[i-1].pins == 10)
+        allPins.add(@pins[i])
+      end
+    end
+
+    return allPins
   end
 end
 
 class Pins
-  protected
   attr_reader :pins
 
-  public
   def initialize(pins)
     @pins = pins
   end
@@ -36,6 +45,15 @@ class Pins
 end
 
 class BowlingTest < Test::Unit::TestCase
+  
+  def test_aSingleSpare
+    game = Game.new()
+    game.throw(Pins.new(9))
+    game.throw(Pins.new(1))
+    game.throw(Pins.new(1))
+    assert_equal(Pins.new(9+1+1+1), game.score, "expected 1 bonus because of the spare")
+  end
+  
   def test_aGameOfOnly1s
     game = Game.new()
     20.times do
