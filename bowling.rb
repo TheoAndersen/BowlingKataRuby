0ee503118bfb67d91b0b09f584a07ca29b163a06
Theo Andersen Carton <andersen.theo@gmail.com>
2015-07-19 20:57:31 +0200

    Added support for a strike

--- bowling.rb
+++ bowling.rb
@@ -7,10 +7,13 @@ class Game
   end
 
   def throw(pins)
-    if @currentFrame.throwOne.nil? &&
-        pins.isStrike == false
+    if @currentFrame.throwOne.nil?
       @currentFrame.throwOne = pins
       @frames.push(@currentFrame)
+
+      if pins.isStrike()
+        @currentFrame = Frame.new()
+      end
     else
       @currentFrame.throwTwo = pins
       @currentFrame = Frame.new()
@@ -28,7 +31,11 @@ class Game
       allPins.add(Pins.new(@frames[i].total))
 
       if i > 0 && @frames[i-1].total == 10
-        allPins.add(@frames[i].throwOne)
+        if @frames[i-1].throwOne.isStrike()
+          allPins.add(Pins.new(@frames[i].total))
+        else
+          allPins.add(@frames[i].throwOne)
+        end
       end
     end
 
@@ -80,30 +87,38 @@ class Frame
 end
 
 class BowlingTest < Test::Unit::TestCase
-  
-   def test_aSingleSpare                                                                  
-     game = Game.new()                                                                    
-     game.throw(Pins.new(9))                                                              
-     game.throw(Pins.new(1))                                                              
-     game.throw(Pins.new(1))                                                              
-     assert_equal(Pins.new(9+1+1+1), game.score, "expected 1 bonus because of the spare") 
-   end                                                                                    
-                                                                                          
-   def test_aGameOfOnly1s                                                                 
-     game = Game.new()                                                                    
-     20.times do                                                                          
-       game.throw(Pins.new(1))                                                            
-     end                                                                                  
-     assert_equal(Pins.new(20), game.score)                                               
-   end                                                                                    
-                                                                                          
-   def test_canThrowAPin                                                                  
-     game = Game.new()                                                                    
-     game.throw(Pins.new(1))                                                              
-     assert_equal(Pins.new(1), game.score)                                                
-   end                                                                                    
 
-   def test_frameCanCalculateTotalScore
+  def test_AStrike
+    game = Game.new()
+    game.throw(Pins.new(10))
+    game.throw(Pins.new(1))
+    game.throw(Pins.new(1))
+    assert_equal(Pins.new(10+2+2), game.score, "expected 2 bonus because of the strike")
+  end
+  
+  def test_aSingleSpare                                                                  
+    game = Game.new()                                                                    
+    game.throw(Pins.new(9))                                                              
+    game.throw(Pins.new(1))                                                              
+    game.throw(Pins.new(1))                                                              
+    assert_equal(Pins.new(9+1+1+1), game.score, "expected 1 bonus because of the spare") 
+  end                                                                                    
+  
+  def test_aGameOfOnly1s                                                                 
+    game = Game.new()                                                                    
+    20.times do                                                                          
+      game.throw(Pins.new(1))                                                            
+    end                                                                                  
+    assert_equal(Pins.new(20), game.score)                                               
+  end                                                                                    
+  
+  def test_canThrowAPin                                                                  
+    game = Game.new()                                                                    
+    game.throw(Pins.new(1))                                                              
+    assert_equal(Pins.new(1), game.score)                                                
+  end                                                                                    
+
+  def test_frameCanCalculateTotalScore
     assert_equal(0, newFrame(nil, nil).total)
     assert_equal(1, newFrame(Pins.new(1), nil).total)
     assert_equal(1, newFrame(nil, Pins.new(1)).total)
